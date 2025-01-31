de8ccb3927d403c3dc3c76be51bd1b4db2e3d182
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-01-31 15:01:00 +1100

    Show modification time when choosing checksums in gen script

--- gen_datapackage.py
+++ gen_datapackage.py
@@ -5,24 +5,27 @@ import json
 import os
 import re
 import requests
+import pathlib
+import datetime
 
 game = input("Enter game: ")
 checksum = None
 dp = None
 try:
-    cache_path = os.path.expandvars(f"%LOCALAPPDATA%/Archipelago/Cache/datapackage/{game}/")
+    cache_path = pathlib.Path(os.path.expandvars(f"%LOCALAPPDATA%/Archipelago/Cache/datapackage/{game}/"))
     # I think this cache comes from the .NET client?  If it exists, it's helpful, if not, we'll just fetch the datapackage from the API.
-    if os.path.exists(cache_path):
-        checksums = [os.path.splitext(f)[0] for f in os.listdir(cache_path)]
+    if cache_path.exists():
+        checksums = [f for f in cache_path.iterdir() if f.is_file()]
         if len(checksums) == 1:
-            checksum = checksums[0]
+            checksum = checksums[0].stem
         else:
             print("Multiple checksums found: ")
             for i, c in enumerate(checksums):
-                print(f"{i}: {c}")
-            checksum = checksums[int(input("Enter index: "))]
+                date = datetime.datetime.fromtimestamp(c.stat().st_mtime)
+                print(f"{i}: {c.stem} ({date})")
+            checksum = checksums[int(input("Enter index: "))].stem
     if checksum:
-        dp = json.load(open(f"{cache_path}{checksum}.json", "r", encoding="utf-8"))
+        dp = json.load(open(f"{cache_path}/{checksum}.json", "r", encoding="utf-8"))
 except Exception as e:
     print(e)
 
