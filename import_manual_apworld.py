e68f232c818d30f73a07583e035545370535d18b
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-02-11 13:06:23 +1100

    Don't delete old data from previous versions of a manual

--- import_manual_apworld.py
+++ import_manual_apworld.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 import zipfile
 
 from os import listdir
@@ -14,19 +15,29 @@ def extract_manual(fname):
     dirs = list(set([os.path.dirname(x) for x in archive.namelist()]))
     top_dir = dirs[0].split('/')[0]
     gamedata = json.load(archive.open(f'{top_dir}/data/game.json'))
-    items = json.load(archive.open(f'{top_dir}/data/items.json'))
+    itemdata = json.load(archive.open(f'{top_dir}/data/items.json'))
 
     game_name = f"Manual_{gamedata['game']}_{gamedata.get('creator',gamedata.get('player'))}"
     game_folder = os.path.join(world_folder, game_name)
     os.makedirs(game_folder, exist_ok=True)
 
     game_file = os.path.join(game_folder, "progression.txt")
+    items = {}
+    try:
+        progressionFile = open(os.path.join(world_folder, game_name, "progression.txt"), "r", encoding="utf-8")
+        text = progressionFile.read()
+        progressionFile.close()
+        for x in text.splitlines():
+            match = re.match(r"(.*): (.*)", x)
+            items[match[1]] = match[2]
+    except FileNotFoundError:
+        pass
 
     with open(game_file, "w+", encoding="utf-8") as f:
         default_filler = gamedata.get('filler_item_name', '')
         if default_filler:
             f.write(f"{gamedata['filler_item_name']}: filler\n")
-        for item in items:
+        for item in itemdata:
             if default_filler and item['name'] == default_filler:
                 continue
             classification = "filler"
@@ -38,7 +49,12 @@ def extract_manual(fname):
                 classification = "progression"
             elif item.get("useful"):
                 classification = "useful"
-            f.write(f"{item['name']}: {classification}\n")
+            items[item['name']] = classification
+
+    with open(os.path.join(world_folder, game_name, "progression.txt"), "w", encoding="utf-8") as f:
+        for item in items:
+            f.write(f"{item}: {items[item]}\n")
+
     archive.close()
 
 if os.path.exists(input_folder):
